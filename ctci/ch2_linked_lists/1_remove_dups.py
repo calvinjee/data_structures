f1d742a1c6922540a2c2026ff23e63f9b39a3412
calvinjee <cal.jee7@gmail.com>
2017-10-04 14:18:27 -0400

    Add 2.1

---
+++ ctci/ch2_linked_lists/1_remove_dups.py
@@ -0,0 +1,36 @@
+# 2.1
+# Remove Dups
+# Write code to remove duplicates from an unsorted linked list
+
+
+def remove_dups(ll):
+    if ll.head is None:
+        return
+
+    node = ll.head
+    seen = set([node.val])
+    while node.next:
+        if node.next.val in seen:
+            node.next = node.next.next
+        else
+            seen.add(node.next.val)
+            node = node.next
+
+    return ll
+
+
+def remove_dups2(ll):
+    if ll.head is None:
+        return
+
+    current = ll.head
+    while current.next:
+        runner = current
+        while runner.next:
+            if current.val == runner.next.val:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+        current = current.next
+
+    return ll
